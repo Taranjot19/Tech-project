6f6ef086a350bdd63c365b8f99260f782fcc4ba6
Taranjot19 <taranjotkaurrandhawa19@gmail.com>
2024-12-16 22:18:30 -0700

    File

---
+++ websitescript.js
@@ -0,0 +1,10 @@
+
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  
+  if (link.href === window.location.href) {
+    link.classList.add('active'); 
+  }
+});
